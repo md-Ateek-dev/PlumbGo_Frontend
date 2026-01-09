c498ba03a1047e39448f2d3b99cef921b04162e8
md-Ateek-dev <mohdateek.dev@gmail.com>
2026-01-10 00:42:01 +0530

    some update

--- src/Services/Client.js
+++ src/Services/Client.js
@@ -11,7 +11,7 @@ import axios from "axios";
 
 // Vite env se baseURL
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+  import.meta.env.VITE_API_BASE_URL || "https://plumbgo-backend.onrender.com";
 
 const Client = axios.create({
   baseURL: API_BASE_URL,
